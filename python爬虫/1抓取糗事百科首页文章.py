4ef5dabd0c0feddcdc5ee59f0c1b7f24938cf042
Coder-chen <18767122273@163.com>
2017-04-29 22:55:25 +0800

    抓取网页并打印

--- python爬虫/1抓取糗事百科首页文章.py
+++ python爬虫/1抓取糗事百科首页文章.py
@@ -1,3 +1,4 @@
+# -*- coding:UTF-8 -*-
 #抓取糗事百科首页文章
 #参考博客：http://python.jobbole.com/81351/
 
@@ -14,19 +15,12 @@ headers = { 'User-Agent' : user_agent }
 try:
     request = urllib2.Request(url, headers=headers)
     response = urllib2.urlopen(request)
-    #data = response.read()
-    #data = data.decode('UTF-8')
-    #data = data.encode(type)
-    #print data
+    data = response.read()
+    data = data.decode('UTF-8')
+    data = data.encode(type)
+    print data
 
-    content = response.read().decode('UTF-8').encode(type)
-    print content
-    pattern = re.compile('<div.*?class="author.*?>.*?<a.*?</a>.*?<a.*?>(.*?)</a>.*?<div.*?class'+'="content".*?title="(.*?)">(.*?)</div>(.*?)<div class="stats.*?class="number">(.*?)</i>',re.S)
-    print 'findall start'
-    items = re.findall(pattern,content)
-    print 'findall end'
-    for item in items:
-        print item[0],item[1],item[2],item[3],item[4]
+
 except urllib2.URLError, e:
     if hasattr(e, "code"):
         print e.code
