03134a1e2d4cad47684f8048d66daa841b169def
Coder-chen <18767122273@163.com>
2017-05-20 22:11:12 +0800

    HTTP基本接入认证

---
+++ python网络数据采集/9穿越网页表单与登录窗口进行采集/9.5处理登录和cookie/HTTPBasicAuth.py
@@ -0,0 +1,7 @@
+#HTTP基本接入认证
+import requests
+from requests.auth import AuthBase
+from requests.auth import HTTPBasicAuth
+auth = HTTPBasicAuth('ryan', 'password')
+r = requests.post(url="http://pythonscraping.com/pages/auth/login.php", auth=auth)
+print(r.text)
