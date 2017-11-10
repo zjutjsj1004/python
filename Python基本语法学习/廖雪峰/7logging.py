07d21a5c5269c095d53e1d948bc78507ef7d2b5e
Coder-chen <18767122273@163.com>
2017-11-10 11:47:27 +0800

    loggin

--- Python基本语法学习/廖雪峰/7logging.py
+++ Python基本语法学习/廖雪峰/7logging.py
@@ -1,5 +1,5 @@
 import logging
-logging.basicConfig(filename = "test.log", level = logging.DEBUG)
+logging.basicConfig(filename = "test.log", level = logging.DEBUG)#需要指定filename和level，不然无法输出
 
-#需要指定，不然无法输出
+
 logging.exception("11111111")
