1b576cbe26660400da9bc3330e3b1e34595d0b85
Coder-chen <18767122273@163.com>
2017-11-10 16:36:52 +0800

    10struct

---
+++ Python基本语法学习/廖雪峰/10struct.py
@@ -0,0 +1,2 @@
+#struct:https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001431955007656a66f831e208e4c189b8a9e9f3f25ba53000
+#https://docs.python.org/3/library/struct.html#format-characters
