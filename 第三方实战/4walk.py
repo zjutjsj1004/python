4fe988a6aacde2d24d52c1f9e535e36b875df0e2
Coder-chen <18767122273@163.com>
2020-02-24 13:05:35 +0800

    1、pilst后缀处理 2、说明 3、文件名和文件夹名等号处理

--- 第三方实战/4walk.py
+++ 第三方实战/4walk.py
@@ -1,10 +1,16 @@
 #开发环境说明：基于Python3开发
-#功能说明:此脚本实现目标文件夹下面，文件夹和文件的一键重命名功能。 目前重命名后的文件名和文件夹名字是原文件夹名和文件名的反转。
-#        脚本执行后，默认在根文件夹(假设根文件夹名字为rootPath)同一目录生成rootPathGen。
-#使用说明: 设置下面三个参数:
+#功能说明:此脚本实现目标文件夹下面，文件夹和文件的一键重命名功能。脚本执行后，默认在根文件夹(假设根文件夹名字为rootPath)同一目录生成rootPathGen。
+#重命名规则: 
+# 1、目前重命名后的文件名和文件夹名字是原文件夹名和文件名的反转。
+# 2、保留原后缀，不过如果后缀是plist,也进行反转为:tsilp
+
+#参数说明: 
 # 1、rootPath 进行重命名的文件根路径
 # 2、EXCLUDEDIRLIST 目标文件夹下需要过滤的文件夹名称
 # 3、EXCULUEFILELIST 目标文件夹下需要过滤的文件名称
+
+#特殊说明:文件夹名或者文件名不能使用等号(=)
+
 rootPath="E:\\Gitlab\\teen"
 EXCLUDEDIRLIST=[".git", ".vscode"]
 EXCULUEFILELIST=[""]
@@ -68,6 +74,10 @@ def genNewDir(oldDir):
     gDict[oldDir] = newDir 
     return newDir
 
+# 字符串特殊处理("\\"-> "/")
+def SpecialThans(str):
+    return str.replace("\\", "/")
+
 print("============开始运行============")
 startTime = time.clock()
 gDict = {}
@@ -116,6 +126,8 @@ for dirpath,dirnames,filenames in g:
                 elif splitFilenameLen == 2:
                     filePrefix = filename.split('.')[0]
                     fileSuffix = filename.split('.')[1]
+                    if fileSuffix == "plist":
+                        fileSuffix = fileSuffix[::-1]
                     fileFullPathAfter = os.path.join(gDict[dirpath],  fun_formatFileName(filePrefix) + "." +fileSuffix)
                 else:
                     fileFullPathAfter = os.path.join(gDict[dirpath], filename)
@@ -128,17 +140,30 @@ for dirpath,dirnames,filenames in g:
                 #os.renames(os.path.join(dirpath,filename), os.path.join(dirpath,  formatDirName + "." +fileSuffix))
 
 print("==========gDict==============")
+bSuccess = True
 f=open(os.path.join(genPath, 'map.txt'), "w",encoding="utf-8")
 print("filename:", f.name)
+f.writelines(["[map]", "\n"])
 for key, value in gDictFile.items():
-    f.writelines([key.replace("\\", "/"), ",", value.replace("\\", "/"), "\n"])
+    f.writelines([SpecialThans(key), ":", SpecialThans(value), "\n"])
+    if key.find("=") != -1:
+        print("[Error]等号不能存在于文件夹名字或者文件名字，请重命名!!  ", key, "--->", value)
+        f.writelines(["[Error]等号不能存在于文件夹名字或者文件名字，请重命名!!", "\n"])
+        bSuccess = False
+        break
     print(key, "--->", value)
 f.close()
 
-ret = os.system('F:\\gocpplua\\awesome-cpp\\CodeSegment\\Debug\\CodeSegment' + " 11111" + " 22222")
-if 0 != ret:
-    print("CodeSegment 执行失败!")
-    pass
+if bSuccess:
+    ret = os.system('F:\\gocpplua\\awesome-cpp\\CodeSegment\\Debug\\CodeSegment' + " 11111" + " 22222")
+    if 0 != ret:
+        bSuccess = False
+        print("[Error]CodeSegment 执行失败!")
 
-print("运行结束!! 耗时统计:", str(time.clock() - startTime))
-print("重命名文件和文件夹请查看下述路径:", genPath)
+costTime = time.clock() - startTime
+if bSuccess:
+    print("重命名文件和文件夹请查看下述路径:", genPath)
+    print("[Success]执行成功!! 耗时统计:", str(costTime))
+else:
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print("[Error]执行失败!! 耗时统计:", str(costTime))
