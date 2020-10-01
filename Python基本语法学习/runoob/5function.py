8ab0bd8d2ba5fd56057d801e7d1512826ae0665c
Amritach222 <47746651+Amritach222@users.noreply.github.com>
2020-10-01 23:03:24 +0545

    Update 5function.py

--- Python基本语法学习/runoob/5function.py
+++ Python基本语法学习/runoob/5function.py
@@ -1,14 +1,17 @@
+#Defining function hello()
 def hello():
     print("hello")
-
+#Calling function for execution
 hello()
-
+#import moduleTest module
 import moduleTest
+#defining ChangeInt() method with a argument
 def ChangeInt( a ):
     a = 10
 
 b = 2
+#function call with passing argument
 ChangeInt(b)
 print(b)
-
+# invoke method of module moduleTest
 moduleTest.moduleTest_1()
