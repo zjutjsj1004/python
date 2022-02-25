22872ba86633088fd2e155458d0870caff785999
chenqi <chenqi1@sensetime.com>
2022-02-25 09:07:48 +0800

    regex

--- Python基本语法学习/廖雪峰/9regex.py
+++ Python基本语法学习/廖雪峰/9regex.py
@@ -11,3 +11,12 @@ t = '19:05:30'
 print('Test:', t)
 m = re.match(r'^(0[0-9]|1[0-9]|2[0-3]|[0-9])\:(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|[0-9])\:(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|[0-9])$', t)
 print(m.groups())
+
+
+urls = re.findall('.*VID_[0-9]{8}_[0-9]{6}', 'VID_20200727_142136_00_062.mp4')
+#urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', 'https://www.yiibai.com/python_text_processing/python_extract_url_from_text.html')
+print(urls)
+if len(urls) == 0:
+  print('error')
+
+print(urls[0])
