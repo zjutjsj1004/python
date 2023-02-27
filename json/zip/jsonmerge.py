0822d3780e3c44eae497495e9db4701b97d8f31b
chenqi1 <chenqi1@sensetime.com>
2023-02-27 09:10:26 +0800

    jsonmerge

--- json/zip/jsonmerge.py
+++ json/zip/jsonmerge.py
@@ -24,23 +24,24 @@ def AnalysisJson(coco_list):
         mergeData["images"] = []
         mergeData["categories"] = []
         mergeData["annotations"] = []
-        for fileTmp in filesTmp:  #遍历当前路径下所有非目录子文件
-            if os.path.splitext(fileTmp)[1] == '.json' and fileTmp != "merge.json": #只获取Json格式的文件
-                with open(os.path.join(dir, fileTmp), encoding="utf-8", mode='r') as f:
-                    # 设置以utf-8解码模式读取文件，encoding参数必须设置，否则默认以gbk模式读取文件，当文件中包含中文时，会报错
-                    temp = json.load(f)      #json格式数据转换为python字典类型
-                    #mergeData["images"].extend(temp["images"])
-                    #mergeData["categories"].extend(temp["categories"])
-                    #mergeData["annotations"].extend(temp["annotations"])
-                    MergeCoco(mergeData["images"], temp["images"])
-                    MergeCoco(mergeData["categories"], temp["categories"])
-                    MergeCoco(mergeData["annotations"], temp["annotations"])
-        
-        if len(mergeData["images"]) == 0:
-            print("error")
-        else:
-            with open(mergejson, "w") as outfile: 
-                json.dump(mergeData, outfile)
+        for _,_,filesTmp in os.walk(os.path.join(dir)): 
+            for fileTmp in filesTmp:  #遍历当前路径下所有非目录子文件
+                if os.path.splitext(fileTmp)[1] == '.json' and fileTmp != "merge.json": #只获取Json格式的文件
+                    with open(os.path.join(dir, fileTmp), encoding="utf-8", mode='r') as f:
+                        # 设置以utf-8解码模式读取文件，encoding参数必须设置，否则默认以gbk模式读取文件，当文件中包含中文时，会报错
+                        temp = json.load(f)      #json格式数据转换为python字典类型
+                        #mergeData["images"].extend(temp["images"])
+                        #mergeData["categories"].extend(temp["categories"])
+                        #mergeData["annotations"].extend(temp["annotations"])
+                        MergeCoco(mergeData["images"], temp["images"])
+                        MergeCoco(mergeData["categories"], temp["categories"])
+                        MergeCoco(mergeData["annotations"], temp["annotations"])
+            
+            if len(mergeData["images"]) == 0:
+                print("error")
+            else:
+                with open(mergejson, "w") as outfile: 
+                    json.dump(mergeData, outfile)
 
 def Check(coco_list):
     for dir in coco_list:
@@ -53,7 +54,7 @@ if __name__ == '__main__':
     coco_list = []
     for root,dirs,files in os.walk(file_path):  # 遍历file_path下所有的子目录及文件
         for dir in dirs:
-            for rootTmp,dirsTmp,filesTmp in os.walk(os.path.join(root, dir)): 
+            for rootTmp,dirsTmp,_ in os.walk(os.path.join(root, dir)): 
                 coco_list.append(os.path.join(rootTmp))
 
     AnalysisJson(coco_list)
