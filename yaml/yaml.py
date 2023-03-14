a50ff4d6b993771fba3c8b4b0a25c58961d4d206
chenqi1 <chenqi1@sensetime.com>
2023-03-14 14:52:39 +0800

    popen_communicate_sys_cmd

--- yaml/yaml.py
+++ yaml/yaml.py
@@ -12,7 +12,7 @@ def save_yaml(yaml_path, config_dict):
 
 
 data = load_yaml('./tools-config.yaml')
-data['mvs_yaml_param']['test'] = 1
+#data['mvs_yaml_param']['test'] = 1
 
 if not data['sfm_yaml_param']: # 不添加这个判定会报错: TypeError: 'NoneType' object does not support item assignment
     data['sfm_yaml_param'] = {}
@@ -23,3 +23,34 @@ data['obj_format'] = ['ply', 'ply1']
 
 print(data)
 save_yaml("./a.yaml", data)
+
+data_mvs = load_yaml('./mvs.yaml')
+param_dict = data.get('mvs_yaml_param')
+size = param_dict.get('max_image_size')
+print('max_image_size:', size)
+for (key, value) in param_dict.items():
+    print(key, value)
+    if key == "max_image_size":
+        value = size
+    
+    data_mvs[key] = value
+
+save_yaml("./b.yaml", data_mvs)
+
+import subprocess
+
+def popen_communicate_sys_cmd(cmd):
+    proc = subprocess.Popen([cmd],
+                            shell=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    stdoutdata, stderrdata = proc.communicate()
+    ret = proc.returncode
+    if ret == 0:
+        ret = [True, stdoutdata.decode('utf-8'), proc.pid, cmd]
+    else:
+        ret = [False, stderrdata.decode('utf-8'), proc.pid, cmd]
+    return ret
+
+ret = popen_communicate_sys_cmd('ls -l /usr/bin')
+print(ret[1])
